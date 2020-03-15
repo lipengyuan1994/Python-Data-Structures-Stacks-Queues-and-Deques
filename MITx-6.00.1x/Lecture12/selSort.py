1b59db703a5c6a434f1ed99d845d89b34fe09f78
Pengyuan Li <lpy199454@gmail.com>
2020-03-15 10:50:36 -0400

    update

---
+++ MITx-6.00.1x/Lecture12/selSort.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 20:32:38 2016
+
+@author: ericgrimson
+"""
+
+def selSort(L):
+    for i in range(len(L) - 1):
+        print(L)
+        minIndx = i
+        minVal= L[i]
+        j = i + 1
+        while j < len(L):
+            if minVal > L[j]:
+                minIndx = j
+                minVal= L[j]
+            j += 1
+        temp = L[i]
+        L[i] = L[minIndx]
+        L[minIndx] = temp
+        
+
+test = [1, 5, 3, 8, 4, 9, 6, 2]
+
+
+
+def newSort(L):
+    for i in range(len(L) - 1):
+        j=i+1
+        while j < len(L):
+            if L[i] > L[j]:
+                temp = L[i]
+                L[i] = L[j]
+                L[j] = temp
+            j += 1
